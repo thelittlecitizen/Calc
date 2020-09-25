45502079504fd5e12d044fc40026cf6c87233bee
thelittlecitizen <thelittlecitizen1@gmail.com>
2020-09-25 13:04:31 +0300

    create calculator with input number in 1 line

--- calc/src/App.js
+++ calc/src/App.js
@@ -6,16 +6,12 @@ function App() {
   const [clear,setClear]=useState(false);//initialize clear screen
 
   useEffect(()=>{
-    document.querySelector('#result').value="";
-  },[])
-  
-  useEffect(()=>{
     if(clear)
     document.querySelector('#result').value="";
   })
 
   const Add=(e)=>{
-    e.preventDefault();
+    e.preventDefault(); 
     if(clear) setClear(false);
     let currentNum=document.querySelector('#num').value
     if(currentNum==='')
@@ -26,11 +22,47 @@ function App() {
       
   }
 
+  const Sub=(e)=>{
+    e.preventDefault(); 
+    if(clear) setClear(false);
+    let currentNum=document.querySelector('#num').value
+    if(currentNum==='')
+    return;
+    let sub= currentSum-parseInt(currentNum);
+    setCurrentSum(sub);
+    document.querySelector('#num').value="";
+      
+  }
+
+  const MUlt=(e)=>{
+    e.preventDefault(); 
+    if(clear) setClear(false);
+    let currentNum=document.querySelector('#num').value
+    if(currentNum==='')
+    return;
+    let mult= currentSum*parseInt(currentNum);
+    setCurrentSum(mult);
+    document.querySelector('#num').value="";
+      
+  }
+
+  const Div=(e)=>{
+    e.preventDefault(); 
+    if(clear) setClear(false);
+    let currentNum=document.querySelector('#num').value
+    if(currentNum==='')
+    return;
+    let div= currentSum/parseInt(currentNum);
+    setCurrentSum(div);
+    document.querySelector('#num').value="";
+      
+  }
+
   const Clear=(e)=>{
     e.preventDefault();
-    console.log('sum:', currentSum);
-    document.querySelector('form').reset();
-    setClear(true);
+    // console.log('sum:', currentSum);
+    // document.querySelector('form').reset();
+    if(clear) setClear(true);
     setCurrentSum(0);
   }
 
@@ -40,9 +72,12 @@ function App() {
         <h1> Little Citizen 1 Calculator</h1>
       </div>
       <form>
-            <input type="text" id="result" value={currentSum}  readOnly />   
+            <input type="text" id="result" value={currentSum}  readOnly placeholder="result" />   
             <input type="text" id="num" placeholder="enter a number" />
             <button onClick={Add}>Add</button>
+            <button onClick={Sub}>Sub</button>
+            <button onClick={MUlt}>MUlt</button>
+            <button onClick={Div}>Div</button>
             <button onClick={Clear}>Clear</button>
       </form>
     </div>
